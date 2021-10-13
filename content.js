496d2936f03935fa650326bd44fb6412d0a79a06
ishagarg1809 <53342167+ishagarg1809@users.noreply.github.com>
2021-10-13 12:29:38 +0530

    html,css,js

---
+++ content.js
@@ -0,0 +1,21 @@
+// chrome.runtime.onMessage.addListener(function (request) {
+//     opencb(request)
+// });
+
+// const targetDiv = document.getElementById("color-blindness");
+// function opencb(){
+//     if(targetDiv.style.display == "none"){
+//         targetDiv.style.display = "block";
+//     }
+// };
+
+// const targetDiv = document.getElementById('color-blindness')
+// const HideDiv = document.getElementById('outer-box')
+
+// document.getElementById('cb-button').addEventListener("click", opencb, false);
+// function opencb(){
+//     if(targetDiv.style.display == "none"){
+//         HideDiv.style.display = "none";
+//         targetDiv.style.display = "block";
+//     }
+// };
---
+++ popup.js
@@ -0,0 +1,31 @@
+// console.log("hello");
+const targetDiv = document.getElementById('color-blindness')
+console.log(targetDiv)
+const HideDiv = document.getElementById('outer-box')
+let cb_btn = document.getElementById("cb-button");
+console.log(cb_btn)
+
+cb_btn.addEventListener("click", function(){
+    console.log("inside opencb")
+    if(targetDiv.style.display == "none"){
+        HideDiv.style.display = "none";
+        targetDiv.style.display = "block";
+    }
+});
+// function opencb(){
+    
+// };
+
+// document.addEventListener('DOMContentLoaded', function () {
+// document.getElementById('cb-button').addEventListener('click', opencb, false)
+// const targetDiv = document.getElementById('color-blindness')
+//     function opencb () {
+//         if(targetDiv.style.display == "none"){
+//             targetDiv.style.display = "block";
+//         }
+//             // var color = document.getElementById("color").value  
+//             // chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+//             //   chrome.tabs.sendMessage(tabs[0].id, color)
+//             // })
+//     }
+// }, false)
