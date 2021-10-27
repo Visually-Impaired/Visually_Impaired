ee5accab269db41ba0a2e7d448d428507500fab2
kalash25 <jainkalash25@gmail.com>
2021-10-27 14:01:04 +0530

    summary get request, and issues with speech synthesis

--- content/content_summary.js
+++ content/content_summary.js
@@ -28,7 +28,11 @@
     summaryButton.style.fontSize = "16px"
 
 
-
+    // fetch('https://regres.in/api/users', {
+    //     mode : 'no-cors',
+    //     credentials : "include",
+    //     method : 'GET'
+    // }).then(res =>console.log(res)).then(data => console.log(data)).catch(error => console.log(error.message))
 
 
 //event listener on button
@@ -44,39 +48,31 @@ function summaryClicked(){
 
     let api_link = "https://c104-117-241-165-48.ngrok.io/api/" + token
 
-    // const xhr = new XMLHttpRequest();
-    // xhr.open("GET", "https://regres.in/api/users")
-    // xhr.responseType = "json"
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", api_link)
+    // xhr.responseType = "text"
     // xhr.withCredentials = true;
-    // // xhr.setRequestHeader("Content_Type", "application/json")
-    // const data_api = ""
-    // xhr.onload = () => {
-    //     data = xhr.response
-    //     console.log(data_api)
-    // }
-    // xhr.send()
-
-
-
-  
-
-
-
-
-
-    // send GET request with the wiki token the backend 
-    let summary_wiki = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32."
-
-    let title_wiki = pageHeading.textContent
-    let data = {
-        tab_id : "content_summary",
-        title : title_wiki,
-        summary : summary_wiki
+    // xhr.setRequestHeader("Content_Type", "application/json")
+    let summary_wiki
+    xhr.onload = () => {
+        summary_wiki = xhr.response
+        // console.log(data_api)
+        let title_wiki = pageHeading.textContent
+        let data = {
+            tab_id : "content_summary",
+            title : title_wiki,
+            summary : summary_wiki
+            }
+        chrome.runtime.sendMessage(data)
     }
+    xhr.send()
+   
+
+    
 
         // chrome.runtime.sendMessage(message)
         // chrome.runtime.sendMessage({from:"content_summary", message:data})
-        chrome.runtime.sendMessage(data)
+        
         // console.log(title)
 
         // export {message}
